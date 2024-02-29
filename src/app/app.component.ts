6ec9c203a8e889d6ce2e8698508bc3e7b73e4e6a
Dizaster0 <a.gangapersaud@gmail.com>
2024-02-29 12:43:11 -0500

    cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { 
+      title: 'Neat Tree',
+      username: '@nature',
+      imageUrl: 'assets/tree.jpeg',
+      content: 'I saw this neat tree today.'
+    },
+    { 
+      title: 'Snowy Mountain',
+      username: '@mountainLover',
+      imageUrl: 'assets/mountain.jpeg',
+      content: 'Here is a picture of a snowy mountain.'
+    },
+    { 
+      title: 'Mountain Biking',
+      username: '@biking12222',
+      imageUrl: 'assets/biking.jpeg',
+      content: 'I did some biking today.'
+    }
+  ]
 }
